ca5c98ef3a2daae389144b802640cd1a333efb32
404411059 <404411059@o365.tku.edu.tw>
2022-03-29 22:29:35 +0800

    import category data as STDIN for remote psql restoration.

--- hw_descriptions/w06/crown_59.sql
+++ hw_descriptions/w06/crown_59.sql
@@ -1,31 +1,42 @@
+-- Postgres Database Dump File
+-- Author: 404411059
+
+set statement_timeout = 0;
 set lock_timeout = 0;
+set idle_in_transaction_session_timeout = 0;
 set client_encoding = 'UTF8';
+set standard_conforming_strings = on;
+set check_function_bodies = false;
+set client_min_messages = warning;
 set row_security = off;
+-- implicit tablespace for `CREATE TABLE`
 set default_tablespace = '';
--- heap allocation for text type
+-- use heap allocation for faster table creation/deletion.
 set default_table_access_method = heap;
+set default_table_drop_method = heap;
 
--- Create database
--- create database crown_59;
-drop table public.category_59;
-
+-- Create category_XX table
+-- note: assuming that you have already created database crown_XX in localhost.
+drop table if exists public.category_59;
 create table public.category_59 (
         id integer not null,
         name text not null,
-        size text,
+        size varchar(10),
         remote_url text,
         local_url text,
         filepath text
 );
 
--- Import Data from local csv file
--- (id, name, size, local_url, remote_url, filepath)
-copy category_59 from '/Users/sheldon/Projects/Workspace/college/teixb2c_database/crown_59/hw_descriptions/w05/category_59.csv' with (format csv);
+-- Import category data from STDIN in csv format.
+-- schema: (id,name,size,local_url,remote_url,filepath)
+copy category_59 from stdin with (format csv);
+1,hats,,https://i.ibb.co/cvpntL1/hats.png,/img/homepage/hats.png,/crown2_59/shop_59/hats
+2,jackets,,https://i.ibb.co/px2tCc3/jackets.png,/img/homepage/jackets.png,/crown2_59/shop_59/jackets
+3,sneakers,,https://i.ibb.co/0jqHpnp/sneakers.png,/img/homepage/sneakers.png,/crown2_59/shop_59/sneakers
+4,womens,large,https://i.ibb.co/GCCdy8t/womens.png,/img/homepage/womens.png,/crown2_59/shop_59/womens
+5,mens,large,https://i.ibb.co/R70vBrQ/men.png,/img/homepage/mens.png,/crown2_59/shop_59/mens
+\.
 
 -- set id as primary key of category_59 table
 alter table only public.category_59 
         add constraint category_59_pkey primary key (id);
-
-alter table public.category_59 owner to tku_404411059;
-
-
