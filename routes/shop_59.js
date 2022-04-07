c8d9d9f158ef6b81d6e65f0e21c2af4e7e90d3eb
404411059 <404411059@o365.tku.edu.tw>
2022-04-07 22:07:53 +0800

    classify data variables by product ID in EJS JSON module.

--- routes/shop_59.js
+++ routes/shop_59.js
@@ -7,10 +7,18 @@ const Shop_59 = require('../models/Shop_59');
 /* GET home page. */
 router.get('/', async (req, res, next) => {
         try {
-                let results = await Shop_59.fetchAll();
-                console.log('Routing results: \n', JSON.stringify(results));
+                let hats = await Shop_59.fetchHats();
+                let jackets = await Shop_59.fetchJackets();
+                let sneakers = await Shop_59.fetchSneakers();
+                let womens = await Shop_59.fetchWomens();
+                let mens = await Shop_59.fetchMens();
+
                 res.render('crown2_59/shop_59', { 
-                        data: results, 
+                        hats_data: hats,
+                        jackets_data: jackets,
+                        sneakers_data: sneakers,
+                        womens_data: womens,
+                        mens_data: mens
                 });
         } catch(err) {
                 console.log('ERROR: ', err);
